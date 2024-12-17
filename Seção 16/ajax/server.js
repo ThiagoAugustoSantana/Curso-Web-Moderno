b0dfc4a146b85925405068cb0564919e1d0d8da5
Thiago Augusto de Santana <thiagoaugustodesantana@hotmail.com>
2024-12-17 16:39:46 -0300

    Aula feita

--- Seção 16/ajax/server.js
+++ Seção 16/ajax/server.js
@@ -29,6 +29,13 @@ app.post('/upload', (req, res) => {
     })
 })
 
+app.post('/formulario', (req, res) => {
+    res.send({
+        ...req.body,
+        id: 1
+    })
+})
+
 // app.get('/teste', (req, res) => res.send('Ok'))
 app.listen(8080, () => console.log('Executando...'))
 
